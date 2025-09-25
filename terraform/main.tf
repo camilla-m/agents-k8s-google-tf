33be4e5620ec3053d32c5cca7b0af66bf9b87f15
camilla-m <camilla-m@outlook.com>
2025-09-25 07:09:27 -0700

    tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -46,14 +46,11 @@ resource "google_container_cluster" "adk_cluster" {
   # We can't create a cluster with no node pool defined, but we want to only use
   # separately managed node pools. So we create the smallest possible default
   # node pool and immediately delete it.
-  remove_default_node_pool = true
   initial_node_count       = 1
 
   # Enable autopilot for better resource management
   enable_autopilot = true
 
-  # Network configuration
-  network    = google_compute_network.vpc.name
   subnetwork = google_compute_subnetwork.subnet.name
 
   # Master authorized networks
